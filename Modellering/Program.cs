7362915f73e48f9d4abed760d894299063158fda
Benjani11 <60339652+Benjani11@users.noreply.github.com>
2020-09-10 16:22:20 +0200

    Jag har typ knappt gjort uppgiften

---
+++ Modellering/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Modellering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Football matchBall = new Football();
+            matchBall.x = 30;
+            matchBall.y = 45;
+            matchBall.z = 0; 
+            matchBall.speed = 15; 
+
+            List<Player> greenTeam = new List<Player>();
+
+            greenTeam.Add(new Player());
+
+
+            List<Player> blackTeam = new List<Player>();
+
+            blackTeam.Add(new Player());
+        }
+
+        public class Football
+        {
+            public int x;
+            public int y;
+            public int z;
+            public int speed;
+             
+        }
+
+        public class Player 
+        {
+           public int x;
+           public int y;
+           public int z; 
+
+           public string team;
+           public string name; 
+        }
+    }
+}
